3fb771ee1d1b52ffc59a445ca63cb8b28941173b
Neil  Antony <neilantony@169-231-40-160.wireless.ucsb.edu>
2023-11-06 23:46:41 -0800

    Implemented Game Logic

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import os
-import sklearn.model_selection
 import keras
 from keras.models import load_model
 import cv2
 import time
 import numpy as np
-
+import random
+import warnings
+warnings.filterwarnings("ignore")
 # Loading model
 model = load_model("./models/model.h5")
 # Defining labels for model prediciton
@@ -13,7 +14,22 @@ categories = ['rock', 'paper', 'scissors']
 
 camera = cv2.VideoCapture(0)
 
-while True:
+# Record Predictions into list to average
+predList = []
+for i in range(50):
+    print("")
+print("Welcome to my computer vision rock paper scissors game!")
+print("type start to begin")
+user_inp = str(input())
+gameState = True if user_inp=="start" else False
+if gameState:
+    print("Position your hand to your right, the program will start in:")
+    # Countdown
+    for i in range(5,0,-1):
+        print(i)
+        time.sleep(1)
+
+while gameState:
     ret, frame = camera.read()
 
     if ret == True:
@@ -29,15 +45,47 @@ while True:
         currentIMG = cv2.resize(roiBW, (50, 50))
 
         currentIMG = np.reshape(currentIMG, (1, 50, 50, 1))
-        pred = model.predict(currentIMG)
+        pred = model.predict(currentIMG, verbose=None)
         pred = np.argmax(pred[0])
+        predList.append(pred)
 
         cv2.putText(frame, categories[pred], (200, 200),
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
         cv2.imshow('video', frame)
-        cv2.imshow('video1', roiBW)
+        #cv2.imshow('video1', roiBW)
     if cv2.waitKey(3) & 0xFF == ord('q'):
+        break
 
+    if len(predList) > 100:
         break
 camera.release()
 cv2.destroyAllWindows()
+
+if gameState:
+    computerMove = categories[random.randint(0,2)] 
+    userMove = categories[round((sum(predList)/len(predList)))]
+    print("The computer used: "+ computerMove)
+    print("You used: " + userMove)
+    
+    if userMove == computerMove:
+        print("Draw!")
+    
+    elif userMove == "rock":
+        if computerMove == "scissors":
+            print("You Win!")
+        else:
+            print("You Loose!")
+
+    elif userMove == "paper":
+        if computerMove == "rock":
+            print("You Win!")
+        else:
+            print("You Loose!")
+
+    else:
+        if computerMove == "paper":
+            print("You Win!")
+        else:
+            print("You Loose!")
+
+print("Thank you for playing!")
